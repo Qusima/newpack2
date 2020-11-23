e7305d8a5ab6a29c81d95379652342db593317ce
Qusima <krzyporz@gmail.com>
2020-11-23 02:06:12 +0100

    poprawa argumentu funkcji

--- R/hello2.R
+++ R/hello2.R
@@ -2,11 +2,12 @@ hello2 <- function(a = 10 , b = 20, d=  2)
 {
   out <- sqrt ( a * b * d/2 + 100+1)
 
-  out <- rnorm(n = round(out,0)) %>%
-    as.data.frame()
+  out <- stats::rnorm(n = base::round(out,0)) %>%
+    out <- base::as.data.frame(out)
 
-  out <- out %>%
-    summarise (mean = mean( ., na.rn = T))
+  out <- dplyr::summarise(out,
+                          mean = base:mean(out,na.rm = T))
+
 
   return(out)
 
